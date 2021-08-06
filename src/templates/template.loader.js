eb2e91e57856e7c4b8a81150488c6684d9efd494
Michael Kastner <kastner@galt.de>
2021-08-06 15:29:44 +0200

    removed loader cache because paths-store keeps loaded dta in memory

--- src/templates/template.loader.js
+++ src/templates/template.loader.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 const log = require('mk-log');
-const Cache = require('./lib/utils/data-cache.js');
 const ids = [
   230, // Hypothekenvergleich
   35, //Studenentkredit
@@ -8,27 +7,15 @@ const ids = [
 
 async function load() {
   try {
-    const cache = Cache.getInstance();
-
-    const apiResult = await cache.pullAsync(this.loopName, async () => {
-      const result = await axios({
-        url: `https://www.fmh.de/api/topics/list-by-ids/${ids.join('-')}`,
-      });
-      return result.data.topics;
+    const apiResult = await axios({
+      url: `https://www.fmh.de/api/topics/list-by-ids/${ids.join('-')}`,
     });
-
-    log.info(
-      'apiResult',
-      apiResult.map((r) => {
-        return { id: r.id, title: r.title };
-      })
-    );
 
     return {
       data: apiResult,
       // iteration loader must always implement eachItem,
       eachVariant: function eachVariant(clos) {
-        const topics = apiResult;
+        const topics = apiResult.data.topics;
         for (let i = 0, l = topics.length; i < l; i++) {
           const topic = topics[i];
           log.info('topic.id', topic.id);
