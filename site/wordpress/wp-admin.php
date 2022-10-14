0355a2dea43717fd7a6678fa252437042cc946ea
mtkastner <kastner@galt.de>
2022-10-14 10:15:23 +0200

    site with static files and wordpress honeypot

---
+++ site/wordpress/wp-admin.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+<form name="loginform" id="loginform" action="/wordpress/wp-login.php" method="post">
+  <p>
+    <label for="user_login">Benutzername oder E-Mail-Adresse</label>
+    <input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="none">
+  </p>
+
+  <div class="user-pass-wrap">
+    <label for="user_pass">Passwort</label>
+    <div class="wp-pwd">
+      <input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20">
+      <button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Passwort anzeigen">
+        <span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+      </button>
+    </div>
+  </div>
+  <p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Angemeldet bleiben</label></p>
+  <p class="submit">
+  <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Anmelden">
+  <input type="hidden" name="redirect_to" value="https://axelkra.us/wp-admin/">
+  <input type="hidden" name="testcookie" value="1">
+  </p>
+</form>
+</body>
+</html>
---
+++ site/wordpress/wp-login.php
@@ -0,0 +1,5 @@
+<html>
+  <body>
+  <h1>You've successfully logged in</h1>
+  </body>
+</html>
